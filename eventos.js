336f19de8d93fd34d2b5125ec32498824d7cbfb8
ignacio <ignaciodavrieux@gmail.com>
2023-08-13 20:35:26 -0300

    solucion boton

--- eventos.js
+++ eventos.js
@@ -1,4 +1,10 @@
 const divElement = document.getElementById('miDiv');
+const detener = document.getElementById("b");
+
+detener.addEventListener("click", function(event) {
+  alert("Clic en el botón");
+  event.stopPropagation(); // Detener la propagación del evento
+});
 
 divElement.addEventListener('click', () => {
   alert('Hola! Soy el div');
